9eb0463a93a15babaf3dafbf4f28f9aaa89d1561
Luka Perkov <luka.perkov@sartura.hr>
2014-11-04 19:50:10 -0500

    intial project import
    Signed-off-by: Luka Perkov <luka.perkov@sartura.hr>

---
+++ src/misc.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Sartura, Ltd.
+ *
+ * Author: Luka Perkov <luka.perkov@sartura.hr>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#ifndef __MISC_RCOND_H__
+#define __MISC_RCOND_H__
+
+#define PROJECT_NAME "rcond"
+
+#define __debug(fmt, ...) do { \
+		fprintf(stderr, "%s: %s (%d): %s: "fmt"\n", PROJECT_NAME, __FILE__, __LINE__, __FUNCTION__, ## __VA_ARGS__ ); \
+        } while (0)
+
+#ifndef typeof
+#define typeof __typeof
+#endif
+
+#ifndef __unused
+#define __unused __attribute__((unused))
+#endif
+
+#endif /* __MISC_RCOND_H__ */
---
+++ src/rcond.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2014 Sartura, Ltd.
+ *
+ * Author: Luka Perkov <luka.perkov@sartura.hr>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <uv.h>
+
+#include "rcond.h"
+#include "misc.h"
+#include "tcp-client.h"
+
+int main(int argc, char *const argv[])
+{
+	int rc;
+
+	/* make sure we can allocate default loop */
+	if (!uv_default_loop()) {
+		__debug("failed to allocate default loop");
+		return EXIT_FAILURE;
+	}
+
+	rc = tcp_client_init();
+	if (rc)	goto exit;
+
+	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+
+	rc = EXIT_SUCCESS;
+exit:
+	uv_loop_close(uv_default_loop());
+
+	return rc;
+}
